39f6e067a647f857dd4d4e52c6e7ddf603dfe4bd
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2018-09-11 17:38:39 +0300

    Initial commit

---
+++ src/a/b/c/Sub2Class1.java
@@ -0,0 +1,10 @@
+package a.b.c;
+
+/**
+ * *******************************
+ * Created by Irina.Petrovskaya on 9/11/2018.
+ * Project: git_submodules_test3_sub2
+ * *******************************
+ */
+public class Sub2Class1 {
+}
